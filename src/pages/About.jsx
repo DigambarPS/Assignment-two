15eab9f719636e8c3e534e73ee2dbf9a3063469b
DigambarPS <74504931+DigambarPS@users.noreply.github.com>
2022-09-11 15:49:21 +0530

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const AboutUs = () => {
+    return(
+        <div className="Container">
+            <h1>About Us</h1>
+            <h3>This is About Us page.</h3>
+        </div>
+    );
+}
+export default AboutUs;
---
+++ src/pages/Support.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Support = () => {
+    return(
+        <div className="Container">
+            <h1>Support</h1>
+            <h3>This is Support page.</h3>
+        </div>
+    );
+}
+export default Support;
